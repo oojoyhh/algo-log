57e3d1998f9392e828a530eeabed09c29e731bf1
oojoyhh <kimhyoju0312@naver.com>
2026-08-19 12:00:51 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.72 ms, Memory: 13.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = []
+    if len(arr) > 1:
+        idx = arr.index(min(arr))
+        del arr[idx]
+        answer = arr
+    
+    else:
+        answer = [-1]
+    return answer
